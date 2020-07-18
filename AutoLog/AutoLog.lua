7028848fc6402e52226c2d4a837f0cef08165700
EKE00372 <eke00372@gmail.com>
2020-07-18 15:35:16 +0800

    enable combatlog when mythic dungens in beta

--- AutoLog/AutoLog.lua
+++ AutoLog/AutoLog.lua
@@ -1,16 +1,19 @@
-local IsInInstance, GetInstanceInfo, LoggingCombat = IsInInstance, GetInstanceInfo, LoggingCombat
+local IsInInstance, GetInstanceInfo, GetBuildInfo = IsInInstance, GetInstanceInfo, GetBuildInfo
+local ILoggingCombat = LoggingCombat
 local GetCVar, SetCVar = GetCVar, SetCVar
 local cvar = GetCVar("advancedCombatLogging")
 
 local function autoLog()
 	local instanceType = select(2, IsInInstance())
 	local difficulty = select(3, GetInstanceInfo())
+	local toc = select(4, GetBuildInfo())
 	
+	-- Force enable advance combat logs
 	if cvar ~= 1 then
 		SetCVar("advancedCombatLogging", 1)
 	end
 	
-	if instanceType == "raid" or difficulty == 8 then
+	if instanceType == "raid" or difficulty == 8 or (toc == 90001 and difficulty == 23) then
 		if not LoggingCombat() then
 			LoggingCombat(true)
 			print("|cff00FF00"..COMBATLOGENABLED.."|r")
