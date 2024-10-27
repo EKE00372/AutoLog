675bf34b8eebe5db46846d12b33d70625e7be482
ZYX <eke00372@gmail.com>
2024-10-27 16:26:41 +0800

    Do expirationTime check when event trigger before function run

--- AutoLog/AutoLog.lua
+++ AutoLog/AutoLog.lua
@@ -1,20 +1,16 @@
 local GetInstanceInfo, LoggingCombat = GetInstanceInfo, LoggingCombat
 local GetCVar, SetCVar = C_CVar.GetCVar, C_CVar.SetCVar
-local last = 0
+local expirationTime
 
 local function autoLog()
 	-- Force enable advance combat logs
 	local cvar = GetCVar("advancedCombatLogging")
-
 	if cvar ~= 1 then
 		SetCVar("advancedCombatLogging", 1)
 	end
 	
 	-- Enable combat logs
 	local _, instanceType, difficultyID = GetInstanceInfo()
-	local currentTime = GetTime()
-
-	if currentTime - last > 0.1 then
 	local loggingStatus = LoggingCombat()
 	if (instanceType == "raid" or difficultyID == 8 or difficultyID == 23) then
 		if loggingStatus == false then
@@ -27,13 +23,21 @@ local function autoLog()
 			print("|cffFF0000"..COMBATLOGDISABLED.."|r")
 		end
 	end
-	last = currentTime
-end
-
 end
 
 local AL = CreateFrame("Frame")
 	AL:RegisterEvent("PLAYER_ENTERING_WORLD")
 	AL:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+	AL:RegisterEvent("PLAYER_DIFFICULTY_CHANGED")
 	AL:RegisterEvent("CHALLENGE_MODE_START")
-	AL:SetScript("OnEvent", autoLog)
+	AL:SetScript("OnEvent", function()
+		if GetTime() - (expirationTime or 0) > 0 then
+			local loggingStatus = LoggingCombat()
+			if loggingStatus == nil then
+				expirationTime = GetTime() + 2
+				C_Timer.After(10, autoLog)
+			else
+				autoLog()
+			end
+		end
+	end)
